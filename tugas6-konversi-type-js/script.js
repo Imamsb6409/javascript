550ee5d5fbcc1364a65102c78fb31d2aaa741624
Imamsb6409 <imam0406@student.abudzar.sch.id>
2025-02-21 08:37:20 +0700

    tugas6-konversi-tipe-js

---
+++ tugas6-konversi-type-js/script.js
@@ -0,0 +1,16 @@
+// latihan 1
+let dressSize = 45;
+// jawab 1
+dressSize = String(dressSize);
+console.log(dressSize);
+
+// jawab 2
+dressSize = +dressSize;
+// bisa juga dressSize = Number(dressSize); tapi bakal lebih panjang
+console.log(dressSize);
+
+// latihan 2
+let isLight = true;
+// jawab 1
+isLight = String(isLight);
+console.log(isLight);
