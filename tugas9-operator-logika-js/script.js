8ee32c1b57370eedaa675829fab7105329afb4a6
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2025-04-23 11:55:24 +0700

    tugas9-operator-logika-js

---
+++ tugas9-operator-logika-js/script.js
@@ -0,0 +1,28 @@
+// soal 1
+let nama = "andi";
+let nilaiMTK = 91,
+  nilaiIPA = 81,
+  nilaiIPS = 91;
+
+if (nilaiMTK >= 91 && nilaiIPA >= 91 && nilaiIPS >= 91) {
+  console.log("andi lulus");
+} else {
+  console.log("andi tidak lulus");
+}
+
+// soal 2
+
+if (nilaiMTK >= 91 || nilaiIPA >= 91 || nilaiIPS >= 91) {
+  console.log("andi lulus");
+} else {
+  console.log("andi tidak lulus");
+}
+
+// soal 3
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+  console.log("anda belum login");
+} else {
+  console.log("anda sudah login");
+}
